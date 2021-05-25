baacc8bbc20b36a2a4f7b5ec22f3df21a0756803
RasedulRasel21 <frasel271@gmail.com>
2021-05-25 15:18:47 +0600

    changes

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -130,3 +130,21 @@
       }
     ]
   });
+
+
+  //scrolling
+  $(document).ready(function() {
+    $(".down").click(function() {
+         $('html, body').animate({
+             scrollTop: $(".up").offset().top
+         }, 1500);
+     });
+    });
+    
+    $(document).ready(function() {
+    $(".up").click(function() {
+         $('html, body').animate({
+             scrollTop: $(".down").offset().top
+         }, 1000);
+     });
+    });
